308a5433024d33aaaf36ee48b03767c74c67c238
kaneko-yuki0627 <241205056@ccmailg.meijo-u.ac.jp>
2025-09-03 16:21:43 +0900

    最終演習まで完了

--- app/src/main/java/jp/ac/meijou/android/s241205056/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205056/MainActivity2.java
@@ -5,16 +5,39 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Optional;
 
 import jp.ac.meijou.android.s241205056.databinding.ActivityMain2Binding;
 import jp.ac.meijou.android.s241205056.databinding.ActivityMainBinding;
 
 public class MainActivity2 extends AppCompatActivity {
     private ActivityMain2Binding binding;
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.textView5.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.textView5.setText("Result: Canceled");
+                    }
+                    default -> {
+                        binding.textView5.setText("Result: Unknown(" + result.getResultCode() + ")");
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +62,20 @@ public class MainActivity2 extends AppCompatActivity {
             intent.setAction(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.yahoo.co.jp"));
             startActivity(intent);
+        });
+
+        //send button
+        binding.button10.setOnClickListener( view -> {
+          //editTextの文字列取得
+          var text = binding.editTextText2.getText().toString();
+          var intent = new Intent(this,MainActivity4.class);
+          intent.putExtra("word",text);
+          startActivity(intent);
+        });
+
+        binding.button11.setOnClickListener( view ->{
+            var intent = new Intent(this, MainActivity4.class);
+            getActivityResult.launch(intent);
         });
     }
 }
