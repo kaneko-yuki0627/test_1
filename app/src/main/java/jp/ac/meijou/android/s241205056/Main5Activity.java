7a7c2d8ebc83fa70a846a6c219a913e8ae4bf1a7
kaneko-yuki0627 <241205056@ccmailg.meijo-u.ac.jp>
2025-09-04 12:21:16 +0900

    OkHttpを表示！！

---
+++ app/src/main/java/jp/ac/meijou/android/s241205056/Main5Activity.java
@@ -0,0 +1,68 @@
+package jp.ac.meijou.android.s241205056;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s241205056.databinding.ActivityMain4Binding;
+import jp.ac.meijou.android.s241205056.databinding.ActivityMainDay41Binding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Main5Activity extends AppCompatActivity {
+    private ActivityMainDay41Binding binding;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+    private final Moshi moshi = new Moshi.Builder().build();
+    private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainDay41Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        var request = new Request.Builder()
+                .url("https://mura.github.io/meijou-android-sample/gist.json")
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                var gist = gistJsonAdapter.fromJson(response.body().source());
+
+                Optional.ofNullable(gist)
+                        .map(g -> g.files.get("OkHttp.txt"))
+                        .ifPresent(gistFile -> {
+                            // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
+                            runOnUiThread(() -> binding.gistView.setText(gistFile.content));
+                        });
+
+            }
+        });
+
+    }
+}
